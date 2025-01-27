0b73d22255b8dbe72c77c770df5b3aa6d6245cc7
Felixdev1996 <felixchizoba01@gmail.com>
2025-01-27 15:03:06 +0100

    omooo

--- src/UI/AboutPage/Aboutpage3.jsx
+++ src/UI/AboutPage/Aboutpage3.jsx
@@ -15,9 +15,11 @@ function Aboutpage3() {
         <p className='text-[27px] md:text-[50px] font-bold mt-[10px] md:mt-[30px]'>Easy Quick Following Steps</p>
         </div>
 
-        <div className=' h-[1900px] md:h-[910px] lg:h-[460px] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 md:gap-x-[23px] px-[10px] md:px-[60px] relative '>
+        <div className=' bg-red-500 h-[1900px] md:h-[910px] lg:h-[460px] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 md:gap-x-[23px] px-[10px] md:px-[60px] relative '>
 
-            <div className='relative flex flex-col items-center justify-center h-[400px] bg-white group clip '>
+          <div className='relative grid grid-cols-1 bg-yellow-300'>
+
+                <div className='relative flex flex-col items-center justify-center h-[400px] bg-white group clip '>
 
                 <button className='bg-[rgb(243,243,243)] py-[25px] px-[25px] rounded-[50%] mb-[15px] absolute top-[25px] group-hover:bg-deepgreen transition duration-[0.6s] '>
                 <FontAwesomeIcon icon={faClipboardCheck}  className='h-[40px] w-[45px] text-deepgreen group-hover:text-white  transition duration-[0.6s]'/>
@@ -26,41 +28,52 @@ function Aboutpage3() {
                 <p className='text-[23px] font-semibold mb-[15px]'>Register For Work</p>
 
                 <p className='w-[300px] md:w-[250px] lg:w-[200px] md:ml-[30px] lg:ml-[35px] text-[rgb(130,134,137)]'>Vulputate libero morbi suscipit curabitur cum aliqam nascetur laoreet nam,</p>
+                </div>
+                
+                <button className='bg-deepgreen py-[13px] px-[20px] rounded-[50%] text-white text-[20px] font-semibold absolute bottom-[15%]
+                right-[43%] md:right-[44%] md:bottom-[10%] lg:bottom-[50px] lg:right-[41%] z-10'>01</button>
 
-               
-            </div>
-            <button className='bg-deepgreen py-[13px] px-[20px] rounded-[50%] text-white text-[20px] font-semibold absolute bottom-[1490px]
-            right-[168px] md:right-[579px] md:bottom-[490px] lg:bottom-[50px] lg:right-[1120px]'>01</button>
+          </div>
 
-            <div className='relative flex flex-col items-center justify-center h-[400px] md:mt-[50px] bg-white group clip'>
+          <div className='relative grid grid-cols-1 bg-yellow-300'>
 
-            <button className='bg-[rgb(243,243,243)] py-[25px] px-[25px] rounded-[50%] mb-[15px] absolute top-[25px] group-hover:bg-deepgreen transition duration-[0.6s] '>
-            <FontAwesomeIcon icon={faMagnifyingGlass}  className='h-[40px] w-[45px] text-deepgreen group-hover:text-white  transition duration-[0.6s]'/>
-            </button>
+              <div className='relative flex flex-col items-center justify-center h-[400px] md:mt-[50px] bg-white group clip'>
 
-            <p className='text-[23px] font-semibold mb-[15px]'>Inspect & Analyze</p>
+              <button className='bg-[rgb(243,243,243)] py-[25px] px-[25px] rounded-[50%] mb-[15px] absolute top-[25px] group-hover:bg-deepgreen transition duration-[0.6s] '>
+              <FontAwesomeIcon icon={faMagnifyingGlass}  className='h-[40px] w-[45px] text-deepgreen group-hover:text-white  transition duration-[0.6s]'/>
+              </button>
 
-            <p className='w-[300px] md:w-[250px] lg:w-[200px] md:ml-[30px] lg:ml-[35px] text-[rgb(130,134,137)]'>Vulputate libero morbi suscipit curabitur cum aliqam nascetur laoreet nam,</p>
+              <p className='text-[23px] font-semibold mb-[15px]'>Inspect & Analyze</p>
 
-
-            </div>
-            <button className='bg-deepgreen py-[13px] px-[20px] rounded-[50%] text-white text-[20px] font-semibold absolute bottom-[1010px]
-            right-[168px] md:right-[209px] md:bottom-[440px] lg:bottom-0 lg:right-[805px]'>02</button>
-
-            <div className='relative flex flex-col items-center justify-center h-[400px] bg-white group clip '>
-
-            <button className='bg-[rgb(243,243,243)] py-[25px] px-[25px] rounded-[50%] mb-[15px] absolute top-[25px] group-hover:bg-deepgreen transition duration-[0.6s] '>
-            <FontAwesomeIcon icon={faGears}  className='h-[40px] w-[45px] text-deepgreen group-hover:text-white  transition duration-[0.6s]'/>
-            </button>
-
-            <p className='text-[23px] font-semibold mb-[15px]'>Work Processing</p>
-
-            <p className='w-[300px] md:w-[250px] lg:w-[200px] md:ml-[30px] lg:ml-[35px] text-[rgb(130,134,137)]'>Vulputate libero morbi suscipit curabitur cum aliqam nascetur laoreet nam,</p>
+              <p className='w-[300px] md:w-[250px] lg:w-[200px] md:ml-[30px] lg:ml-[35px] text-[rgb(130,134,137)]'>Vulputate libero morbi suscipit curabitur cum aliqam nascetur laoreet nam,</p>
 
 
-            </div>
-            <button className='bg-deepgreen py-[13px] px-[20px] rounded-[50%] text-white text-[20px] font-semibold absolute bottom-[540px]
-            right-[168px] md:right-[579px] md:bottom-[40px] lg:right-[490px] lg:bottom-[50px]'>03</button>
+              </div>
+              <button className='bg-deepgreen py-[13px] px-[18px] rounded-[50%] text-white text-[20px] font-semibold absolute bottom-[13%]
+              right-[44%] md:right-[43%] md:bottom-[0%] lg:bottom-[0%] lg:right-[41%] z-20'>02</button>
+            
+          </div>
+
+          <div className='relative grid grid-cols-1 bg-yellow-300'>
+
+              <div className='relative flex flex-col items-center justify-center h-[400px] bg-white group clip '>
+
+              <button className='bg-[rgb(243,243,243)] py-[25px] px-[25px] rounded-[50%] mb-[15px] absolute top-[25px] group-hover:bg-deepgreen transition duration-[0.6s] '>
+              <FontAwesomeIcon icon={faGears}  className='h-[40px] w-[45px] text-deepgreen group-hover:text-white  transition duration-[0.6s]'/>
+              </button>
+
+              <p className='text-[23px] font-semibold mb-[15px]'>Work Processing</p>
+
+              <p className='w-[300px] md:w-[250px] lg:w-[200px] md:ml-[30px] lg:ml-[35px] text-[rgb(130,134,137)]'>Vulputate libero morbi suscipit curabitur cum aliqam nascetur laoreet nam,</p>
+
+
+              </div>
+              <button className='bg-deepgreen py-[13px] px-[19px] rounded-[50%] text-white text-[20px] font-semibold absolute bottom-[12%]
+              right-[43%] md:right-[41.8%] md:bottom-[10%] lg:right-[40%] lg:bottom-[10%]'>03</button>
+                
+          </div>
+
+          <div className='relative grid grid-cols-1 bg-yellow-300'>
 
             <div className='relative flex flex-col items-center justify-center h-[400px] md:mt-[50px] bg-white group clip'>
 
@@ -74,9 +87,15 @@ function Aboutpage3() {
 
 
             </div>
-            <button className='bg-deepgreen py-[13px] px-[20px] rounded-[50%] text-white text-[20px] font-semibold absolute bottom-[60px]
-            right-[168px] md:bottom-0 md:right-[209px] lg:bottom-[0px] lg:right-[172px]'>04</button>
+            <button className='bg-deepgreen py-[13px] px-[19px] rounded-[50%] text-white text-[20px] font-semibold absolute bottom-[12%]
+            right-[43%] md:bottom-0 md:right-[43%] lg:bottom-[0px] lg:right-[40%]'>04</button>
 
+          </div>
+           
+
+            
+
+           
         </div>
 
       </div>
